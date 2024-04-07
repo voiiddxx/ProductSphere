d9fd55d757ce98f5177c1ff68d13f9482ca94790
void <95859137+voiiddxx@users.noreply.github.com>
2024-04-07 21:49:07 +0530

    Setting up the folder structure

---
+++ app/(root)/(auth)/login/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const page = () => {
+  return (
+    <div>
+      <h1>this is login page </h1>
+    </div>
+  )
+}
+
+export default page
---
+++ app/(root)/(auth)/register/register.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const register = () => {
+  return (
+    <div>
+        this is register page 
+      
+    </div>
+  )
+}
+
+export default register

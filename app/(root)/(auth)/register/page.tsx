a80e806346ad57f3148fbb4e9f6a20672a59f23e
void <95859137+voiiddxx@users.noreply.github.com>
2024-04-07 22:02:10 +0530

    Sorted some issues

---
+++ app/(root)/(auth)/register/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const page = () => {
+  return (
+    <div>
+      this is register page
+    </div>
+  )
+}
+
+export default page
--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,15 @@
 import { Button } from "@/components/ui/button";
+import Link from "next/link";
 
 
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
     this is home page for now
-    <div className="flex gap-5" ><Button>Login Page</Button>
-    <Button>Register Page</Button></div>
+    <div className="flex gap-5" >
+      <Link href={`/login`} ><Button>Login Page</Button></Link>
+      <Link href={`/register`} ><Button>Register Page</Button></Link>
+  </div>
     </main>
   );
 }

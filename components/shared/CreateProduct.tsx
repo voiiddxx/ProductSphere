9b0ff03f97c6eb5df5802f54f7e3c05e103e986e
void <95859137+voiiddxx@users.noreply.github.com>
2024-04-12 21:41:21 +0530

    added som elements

--- components/shared/CreateProduct.tsx
+++ components/shared/CreateProduct.tsx
@@ -1,5 +1,5 @@
 "use client";
-import { Blocks, ChevronRight, ChevronsRightLeft, Hash } from "lucide-react";
+import { Blocks, CalendarCheck, ChevronRight, ChevronsRightLeft, Hash } from "lucide-react";
 import React from "react";
 import { z } from "zod";
 import { zodResolver } from "@hookform/resolvers/zod";
@@ -22,6 +22,10 @@ import {
   SelectTrigger,
   SelectValue,
 } from "@/components/ui/select"
+import { Textarea } from "@/components/ui/textarea"
+import { Calendar } from "@/components/ui/calendar"
+
+
 
 
 
@@ -99,6 +103,51 @@ const CreateProduct = () => {
                   )}
                 />
               </div>
+
+
+              {/* second row */}
+
+            <div>
+            <FormField
+                  control={form.control}
+                  name="username"
+                  render={({ field }) => (
+                    <FormItem className="w-full">
+                      <FormLabel>Product Visibility</FormLabel>
+                      <FormControl>
+                      <Textarea className="h-[150px]" placeholder="Platform build to enhance the product building experience in team , using this product you can enhanced the experince of product building experince⚡" />
+                      </FormControl>
+
+                      <FormMessage />
+                    </FormItem>
+                  )}
+                />
+            </div>
+              {/* third row */}
+                  <div className="w-full flex gap-4" >
+                    <div className="w-full" >
+                      <p className="font-medium text-sm mb-2" >Start date</p>
+                    <div className="w-full h-10 flex rounded-lg border items-center px-2 text-zinc-500" >
+                      <CalendarCheck size={17} strokeWidth={1.5} />
+                      <p className="text-sm ml-3" >Thursday 5 nov , 2021</p>
+                      
+                    </div>
+                    </div>
+                    <div className="w-full" >
+                      <p className="font-medium text-sm mb-2" >Product Deadline</p>
+                    <div className="w-full h-10 flex rounded-lg border items-center px-2 text-zinc-500" >
+                      <CalendarCheck size={17} strokeWidth={1.5} />
+                      <p className="text-sm ml-3" >Thursday 5 nov , 2021</p>
+                      
+                    </div>
+                    </div>
+                    
+                    
+                  </div>
+
+
+              {/* fourth row */}
+              {/* fifth row */}
 
               <Button type="submit">Submit</Button>
             </form>

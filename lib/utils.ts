2ad41df6724f0107f8f41728a2b634c100a35997
void <95859137+voiiddxx@users.noreply.github.com>
2024-04-17 17:35:32 +0530

    Feat: = -- Upload fine to cludinary

--- lib/utils.ts
+++ lib/utils.ts
@@ -1,6 +1,28 @@
+import axios from 'axios';
 import { type ClassValue, clsx } from "clsx"
 import { twMerge } from "tailwind-merge"
 
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
 }
+
+
+
+
+const uploadDataonCloudinary = async (datatoUpload : any) => {
+    try {
+        if(datatoUpload!=null){
+          let formdata = new FormData();
+          formdata.append("file" , datatoUpload[0]);
+          formdata.append("upload_preset" , "oahtirqz");
+          const response = await axios.post("https://api.cloudinary.com/v1_1/oahtirqz/upload" , formdata ,);
+          return response.data.url;
+        }
+        else{
+            throw new Error("No File selected");
+        }
+    } catch (error) {
+        console.log(error);       
+    }
+}
+export default uploadDataonCloudinary

48ad0578a048a05e66c5071e9c02d9736eaf8784
void <95859137+voiiddxx@users.noreply.github.com>
2024-04-17 10:58:59 +0530

    added types for creating the product

--- types/index.ts
+++ types/index.ts
@@ -10,4 +10,21 @@ export type CreateUseronSuperbaseParams = {
 }
 
 
+export type createProductParams = {
+    name:string
+    detail:string
+    category:string
+    productlogo:string
+    productcolor:string
+    endDate: Date
+    startDate: Date
+    url:string
+    visibility:string
+    productdocumentone?:string
+    productdocumenttwo?:string
+    ownerId:any
+    members?:[]
+}
+
+
 

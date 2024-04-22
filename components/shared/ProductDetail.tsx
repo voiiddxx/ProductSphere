4a4c5ceeec4122e8af3d76c70168affc657988a8
void <95859137+voiiddxx@users.noreply.github.com>
2024-04-22 10:26:53 +0530

    Added Product detail Page

--- components/shared/ProductDetail.tsx
+++ components/shared/ProductDetail.tsx
@@ -1,6 +1,7 @@
 "use client"
 
 import { getProductWithProductIdAction } from "@/lib/actions/product.action";
+import Image from "next/image";
 import React, { useEffect, useState } from "react";
 
 type ProductDetailProps = {
@@ -22,7 +23,35 @@ const ProductDetail = ({ id }: ProductDetailProps) => {
     getProduct();
   }, []);
 
-  return <div>This is product details .tsx </div>;
+  return (
+    <div className="h-screen w-full px-12" >
+        {/* navbar */}
+        <div className="h-20 w-full border-b" >
+        </div>
+
+
+        {/* heading bar tab */}
+        <div className="h-40   px-5 py-5  w-full bg-indigo-700 rounded-lg mt-5" >
+            {/* upper bar details */}
+            <div className="flex justify-between items-center" >
+                <div>
+                <p className="text-xl text-white font-semibold" >ProductSphere</p>
+                <p className="text-sm text-zinc-400" >Saas</p>
+                </div>
+
+                <div>
+                    <div className="h-28 w-28 bg-white rounded-lg" >
+                        <Image className="h-28 w-28 object-cover rounded-lg"   src="http://res.cloudinary.com/drqhibhiv/image/upload/v1713601228/v8skgxg51aq1iosf34os.svg" height={1500} width={1500} alt="productlogo" />
+
+                    </div>
+                </div>
+                
+            </div>
+
+        </div>
+
+    </div>
+  );
 };
 
 export default ProductDetail;

2907f69aaa01def90d0fa6e0641e728831d6dd83
void <95859137+voiiddxx@users.noreply.github.com>
2024-04-27 05:32:39 +0530

    Added scroll area for adding team mebers

--- components/shared/Addmemebr.tsx
+++ components/shared/Addmemebr.tsx
@@ -13,6 +13,7 @@ import {
 } from "@/components/ui/alert-dialog";
 import { GitBranch, Network } from "lucide-react";
 import { getAllUsers } from "@/lib/actions/user.action";
+import { ScrollArea } from "@/components/ui/scroll-area";
 
 type AddmemebrsProps = {
   productId: number;
@@ -48,6 +49,15 @@ const Addmemebr = ({ productId }: AddmemebrsProps) => {
             <p className="text-xs font-medium">Select members</p>
 
             {/* mapping of all the users */}
+            <ScrollArea className="h-[200px] w-[350px] rounded-md border p-4">
+              Jokester began sneaking into the castle in the middle of the night
+              and leaving jokes all over the place: under the king's pillow, in
+              his soup, even in the royal toilet. The king was furious, but he
+              couldn't seem to stop Jokester. And then, one day, the people of
+              the kingdom discovered that the jokes left by Jokester were so
+              funny that they couldn't help but laugh. And once they started
+              laughing, they couldn't stop.
+            </ScrollArea>
           </AlertDialogDescription>
         </AlertDialogHeader>
         <AlertDialogFooter>

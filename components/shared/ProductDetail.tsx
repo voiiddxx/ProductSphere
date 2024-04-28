d24838f2bbbf6ff0b15a30963fdfab4cdd31c6d9
void <95859137+voiiddxx@users.noreply.github.com>
2024-04-28 12:34:23 +0530

    Added some more changes in the ui component fo product details

--- components/shared/ProductDetail.tsx
+++ components/shared/ProductDetail.tsx
@@ -27,7 +27,6 @@ import {
   AlertDialogTitle,
   AlertDialogTrigger,
 } from "@/components/ui/alert-dialog";
-import { Textarea } from "../ui/textarea";
 import { AddReviewAction } from "@/lib/actions/review.action";
 
 type ProductDetailProps = {
@@ -62,8 +61,6 @@ const makers = [
 ];
 
 
-
-const starsShow = [1, 2, 3, 4, 5];
 
 const ProductDetail = ({ id }: ProductDetailProps) => {
   const [reviewRate, setreviewRate] = useState<number>(0);
@@ -109,24 +106,26 @@ const ProductDetail = ({ id }: ProductDetailProps) => {
     getProduct();
   }, []);
 
-  // const JoinTeamNow = async () => {
-  //   const token = localStorage.getItem("x-auth-id");
-  //   const userId = +token!;
-  //   const proId = +Product.productId;
-  //   console.log("userId :" , userId , "product Id: " , proId);
+  const JoinTeamNow = async () => {
+    const token = localStorage.getItem("x-auth-id");
+    const userId = +token!;
+    const proId = +Product.productId;
+    console.log("userId :" , userId , "product Id: " , proId);
 
-  //   try {
-  //     const res = await JoinProductAction({productCode:productCode , productId:proId , userdId:userId});
-  //     if(res){
-  //       console.log("Join Res: " , res);
-  //     }else{
-  //       console.log("Some error found");
+    try {
+      const res = await JoinProductAction({productCode:productCode , productId:proId , userdId:userId});
+      if(res){
+        console.log("Join Res: " , res);
+      }else{
+        console.log("Some error found");
 
-  //     }
-  //   } catch (error) {
-  //     console.log(error);
-  //   }
-  // };
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  if(Product == null) return <div>please wait</div>
 
   return (
     <div className="h-screen w-full px-12">
@@ -147,7 +146,7 @@ const ProductDetail = ({ id }: ProductDetailProps) => {
           </div>
           {/* text div */}
           <div>
-            <h1 className="text-4xl font-bold text-zinc-900" style={{color:Product.productcolor}} >ProductSphere</h1>
+            <h1 className="text-4xl font-bold text-zinc-900" style={{color:Product.productcolor}} >{Product.name}</h1>
             <p className="mt-1 text-zinc-600 text-sm">
               Software for managing the product
             </p>
@@ -171,21 +170,53 @@ const ProductDetail = ({ id }: ProductDetailProps) => {
         {/* right div */}
         <div className="flex gap-2">
           <Button
-            className="border rounded-full text-indigo-700 h-14 w-14" style={{color:Product.productcolor}}
+            className="border rounded-full text-indigo-700 h-10 w-10" style={{color:Product.productcolor}}
             size={"icon"}
             variant={"link"}
           >
             <div>
-              <Triangle />
-              <p>15</p>
+              <Triangle size={15} />
+              <p className="text-xs font-medium" >15</p>
             </div>
           </Button>
           <Button variant={"outline"} size={"lg"}>
             Visit Website
           </Button>
-          <Button className="bg-indigo-700" style={{backgroundColor:Product.productcolor}}  size={"lg"}>
-            Join Product Team
+          <div className="">
+        <Dialog>
+          <DialogTrigger>
+              <Button className="bg-indigo-700" style={{backgroundColor:Product.productcolor}}  size={"lg"}>
+            Join Product
           </Button>
+          </DialogTrigger>
+          <DialogContent>
+            <DialogHeader>
+              <DialogTitle className="text-sm font-medium"  >Join Team of {Product.name}</DialogTitle>
+              <DialogDescription>
+                <p className="text-xs font-medium" >Join team of  and build in team</p>
+
+                <div>
+                  <input onChange={(e)=>{
+                    setproductCode(e.target.value);
+                  }} className="outline-none border-none mt-5 font-medium text-2xl text-zinc-900 placeholder:text-zinc-500" placeholder="Enter Secret Code" />
+                </div>
+
+                {/* buttons */}
+                <div className="flex mt-10 justify-end gap-2" >
+                  <Button variant={"outline"} >Cancel</Button>
+                  <Button onClick={JoinTeamNow}  className="bg-indigo-700" style={{backgroundColor:Product.productcolor}}>Join Now</Button>
+
+                </div>
+                {/* product join dialogue box */}
+
+              </DialogDescription>
+            </DialogHeader>
+          </DialogContent>
+        </Dialog>
+      </div>
+          {/* <Button className="bg-indigo-700" style={{backgroundColor:Product.productcolor}}  size={"lg"}>
+            
+          </Button> */}
         </div>
       </div>
 
@@ -207,7 +238,7 @@ const ProductDetail = ({ id }: ProductDetailProps) => {
           <div className=" flex gap-5 items-center mt-4">
             <div className="px-4 py-2 border rounded-md flex gap-2 items-center">
               <div className="h-10 w-10 border rounded-sm flex items-center justify-center">
-                <File size={20} className="text-indigo-700" />
+                <File size={20} className="text-indigo-700" style={{color:Product.productcolor}} />
               </div>
               <h1 className="font-medium text-zinc-900 text-sm">
                 ProductSphere details
@@ -215,7 +246,7 @@ const ProductDetail = ({ id }: ProductDetailProps) => {
             </div>
             <div className="px-4 py-2 border rounded-md flex gap-2 items-center">
               <div className="h-10 w-10 border rounded-sm flex items-center justify-center">
-                <File size={20} className="text-indigo-700" />
+                <File size={20} className="text-indigo-700" style={{color:Product.productcolor}} />
               </div>
               <h1 className="font-medium text-zinc-900 text-sm">
                 Team Members Data
@@ -232,30 +263,15 @@ const ProductDetail = ({ id }: ProductDetailProps) => {
         </p>
 
         <div className="mr-96 flex gap-5 flex-wrap mt-5">
-          <div className="px-6 py-2 border rounded-full flex justify-center items-center gap-2">
-            <Sparkle className="text-indigo-700" strokeWidth={1.5} size={18} />
-            <p className="text-indigo-700 font-medium">TypeScript</p>
-          </div>
-          <div className="px-6 py-2 border rounded-full flex justify-center items-center gap-2">
-            <Sparkle className="text-indigo-700" strokeWidth={1.5} size={18} />
-            <p className="text-indigo-700 font-medium">Nextjs</p>
-          </div>
-          <div className="px-6 py-2 border rounded-full flex justify-center items-center gap-2">
-            <Sparkle className="text-indigo-700" strokeWidth={1.5} size={18} />
-            <p className="text-indigo-700 font-medium">javascript</p>
-          </div>
-          <div className="px-6 py-2 border rounded-full flex justify-center items-center gap-2">
-            <Sparkle className="text-indigo-700" strokeWidth={1.5} size={18} />
-            <p className="text-indigo-700 font-medium">PostgresSql</p>
-          </div>
-          <div className="px-6 py-2 border rounded-full flex justify-center items-center gap-2">
-            <Sparkle className="text-indigo-700" strokeWidth={1.5} size={18} />
-            <p className="text-indigo-700 font-medium">Vercel</p>
-          </div>
-          <div className="px-6 py-2 border rounded-full flex justify-center items-center gap-2">
-            <Sparkle className="text-indigo-700" strokeWidth={1.5} size={18} />
-            <p className="text-indigo-700 font-medium">Shadcn-ui</p>
-          </div>
+          {
+            Product.skills.map((curr:any)=>{
+              return <div className="px-6 py-2 border rounded-full flex justify-center items-center gap-2">
+              <Sparkle className="text-indigo-700" style={{color:Product.productcolor}} strokeWidth={1.5} size={18} />
+              <p className="text-indigo-700 font-medium" style={{color:Product.productcolor}} >{curr}</p>
+            </div>
+            })
+          }
+          
         </div>
       </div>
 
@@ -269,15 +285,17 @@ const ProductDetail = ({ id }: ProductDetailProps) => {
         {/* mapping of the team mebers */}
         <div className="mr-96 border-b py-4">
           <div className="flex gap-8 flex-wrap mt-5">
-            {makers.map((curr: any) => {
+            {Product.members.map((curr: any) => {
               return (
                 <div
                   className="h-20 w-96 rounded-full  flex items-center gap-2
                   "
                 >
-                  <div className="h-14 w-14 bg-slate-500 rounded-full"></div>
+                  <div className="h-10 w-10 bg-slate-500 rounded-full">
+                    <Image src={curr.avatar} className="h-10 w-10 rounded-full object-cover" height={900} width={900} alt="userslogo" />
+                  </div>
                   <div>
-                    <h1 className="font-medium text-indigo-700">{curr.name}</h1>
+                    <h1 className="font-medium text-indigo-700" style={{color:Product.productcolor}} >{curr.username}</h1>
                     <p className="text-sm font-medium text-zinc-600">
                       {curr.email}
                     </p>
@@ -293,14 +311,14 @@ const ProductDetail = ({ id }: ProductDetailProps) => {
 
       <div className=" py-5 border-b px-20">
         <h1 className="text-xl font-medium text-zinc-900">
-          ProductSphere Reviews (10){" "}
+          ProductSphere Reviews ({AllReviews.length}){" "}
         </h1>
         <p className="text-zinc-600 text-sm font-medium">
           Chcekout all the reviews given by users
         </p>
         <AlertDialog>
           <AlertDialogTrigger>
-            <Button size={"lg"} className="bg-indigo-700 mt-4 mb-8">
+            <Button size={"lg"} className="bg-indigo-700 mt-4 mb-8" style={{backgroundColor:Product.productcolor}}>
               Leave a review
             </Button>
           </AlertDialogTrigger>

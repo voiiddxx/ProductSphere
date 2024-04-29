3704d58544f78a9e228427b1e7278334631f669c
void <95859137+voiiddxx@users.noreply.github.com>
2024-04-29 17:50:53 +0530

    changed task detail data from static to dynamic data

--- components/shared/TaskDetail.tsx
+++ components/shared/TaskDetail.tsx
@@ -75,8 +75,8 @@ const TaskDetail = ({data}:taskData) => {
             <div className="w-3/5 h-full border-r" >
 
                 {/* headibg */}
-                <h1 className="text-5xl font-bold mt-2" >Design System</h1>
-                <p className="text-sm font-medium mt-4 -tracking-wide leading-relaxed text-zinc-600" >The design systrem for our current application little improvement , have to add some additinols features for enhanced experince</p>
+                <h1 className="text-5xl font-bold mt-2" >{data.title}</h1>
+                <p className="text-sm font-medium mt-4 -tracking-wide leading-relaxed text-zinc-600" >{data.desc}</p>
 
 
                 {/* sub detail div */}
@@ -85,7 +85,7 @@ const TaskDetail = ({data}:taskData) => {
                     <div className="flex gap-16 items-center" >
                         <p className="text-zinc-700 font-medium text-sm" >Status:</p>
                         <div className="flex justify-center items-center px-4 py-1 bg-yellow-100 rounded-full">
-                            <p className="text-yellow-600 text-sm font-medium" >Under Development</p>
+                            <p className="text-yellow-600 text-sm font-medium" >{data.status}</p>
                         </div>
                     </div>
 
@@ -94,7 +94,7 @@ const TaskDetail = ({data}:taskData) => {
                         <p className="text-zinc-700 font-medium text-sm" >Assigned to:</p>
                         <div className="flex justify-center items-center  rounded-full gap-4">
                            {
-                            memebr.map((curr:any)=>{
+                            data.AssignMembers.map((curr:any)=>{
                                 return <div className="px-4 py-1 flex  items-center gap-2 bg-slate-100 rounded-full">
                                     <div className="h-6 w-6 rounded-full bg-indigo-800" >
                                         <Image src={curr.avatar} className="h-6 w-6 object-cover rounded-full" height={900} width={900} alt="userimage" />
@@ -112,7 +112,7 @@ const TaskDetail = ({data}:taskData) => {
                     <div className="flex gap-16 items-center mt-5" >
                         <p className="text-zinc-700 font-medium text-sm" >Due date</p>
                         <div className="flex justify-center items-center px-4 py-1  rounded-full">
-                            <p className="text-zinc-800 font-semibold" >5 April , 2025</p>
+                            <p className="text-zinc-800 font-semibold" >{data.dueDate}</p>
                         </div>
                     </div>
 
@@ -121,7 +121,7 @@ const TaskDetail = ({data}:taskData) => {
                         <p className="text-zinc-700 font-medium text-sm" >Priority:</p>
                         <div className="flex justify-center items-center px-4 py-1 bg-red-100 rounded-full">
                             <Dot className="text-red-600" absoluteStrokeWidth strokeWidth={8} />
-                            <p className="text-red-600 text-sm font-medium" >Very High</p>
+                            <p className="text-red-600 text-sm font-medium" >{data.priority}</p>
                         </div>
                     </div>
 
@@ -137,7 +137,7 @@ const TaskDetail = ({data}:taskData) => {
                           </div>
                            <div>
                             <p className="text-sm font-medium" >Void.tsx</p>
-                            <p className="text-xs font-medium italic mt-2 text-zinc-600" >Lorem, ipsum dolor sit amet consectetur adipisicing elit. Est fugiat vel minus sint aliquam modi ipsum deserunt doloribus omnis fuga.</p>
+                            <p className="text-xs font-medium italic mt-2 text-zinc-600" >{data.comment}</p>
                            </div>
                     </div>
                 </div>
@@ -174,7 +174,7 @@ const TaskDetail = ({data}:taskData) => {
 
                 <div className="flex gap-1 flex-wrap mt-5 border-b pb-4" >
                     {
-                        tags.map((curr:any)=>{
+                        data.tags.map((curr:any)=>{
                             return <div className=" px-3 py-2 border rounded-sm flex justify-center items-center gap-2" >
                                 <Dot className="text-green-600"  absoluteStrokeWidth strokeWidth={8} />
                                 <p className="text-gray-700 text-sm font-medium" >{curr}</p>
@@ -190,8 +190,9 @@ const TaskDetail = ({data}:taskData) => {
                     <p className="text-sm font-medium text-zinc-600 mt-4" >Created By</p>
                     <div className="flex gap-2 items-center mt-3" >
                     <div className="h-8 w-8 bg-slate-400 rounded-full" >
+                      <Image src={data.createdBy.avatar} className="h-8 w-8 rounded-full object-cover"  height={900} width={900} alt="ownerimage" />
                     </div>
-                    <p className="text-sm font-medium" >Nikhil Kumar</p>
+                    <p className="text-sm font-medium" >{data.createdBy.username}</p>
                     </div>
 
                 </div>

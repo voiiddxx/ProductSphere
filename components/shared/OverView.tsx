cb043dfc4502380ea0a0555f72375d0f1ef85327
void <95859137+voiiddxx@users.noreply.github.com>
2024-05-01 04:11:08 +0530

    Added upvotes section

--- components/shared/OverView.tsx
+++ components/shared/OverView.tsx
@@ -12,6 +12,7 @@ import {
 } from "lucide-react";
 import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
 import { Star } from "lucide-react";
+import { AnimatedTooltip } from "../ui/animated-tooltip";
 
 const users = [
   {
@@ -38,6 +39,38 @@ const users = [
     username: "void",
     email: "void@gmail.com",
   },
+];
+
+const people = [
+  {
+    id: 1,
+    name: "John Doe",
+    designation: "Software Engineer",
+    image:
+      "https://images.unsplash.com/photo-1599566150163-29194dcaad36?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=3387&q=80",
+  },
+  {
+    id: 2,
+    name: "Robert Johnson",
+    designation: "Product Manager",
+    image:
+      "https://images.unsplash.com/photo-1535713875002-d1d0cf377fde?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8YXZhdGFyfGVufDB8fDB8fHww&auto=format&fit=crop&w=800&q=60",
+  },
+  {
+    id: 3,
+    name: "Jane Smith",
+    designation: "Data Scientist",
+    image:
+      "https://images.unsplash.com/photo-1580489944761-15a19d654956?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NXx8YXZhdGFyfGVufDB8fDB8fHww&auto=format&fit=crop&w=800&q=60",
+  },
+  {
+    id: 4,
+    name: "Emily Davis",
+    designation: "UX Designer",
+    image:
+      "https://images.unsplash.com/photo-1438761681033-6461ffad8d80?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTB8fGF2YXRhcnxlbnwwfHwwfHx8MA%3D%3D&auto=format&fit=crop&w=800&q=60",
+  },
+  
 ];
 
 const feedbacks = [
@@ -299,6 +332,17 @@ const OverView = () => {
               </div>
             </div>
           </TabsContent>
+          {/* upvotes content */}
+          <TabsContent value="Upvotes" >
+                <div className="pt-5 px-4 py-4" >
+                <p className="text-2xl font-medium text-zinc-900" >Folks</p>
+                </div>
+                <div className="flex gap-3 flex-wrap w-full mt-5" >
+                <AnimatedTooltip items={people} />
+                </div>
+
+          </TabsContent>
+          
         </Tabs>
       </div>
       {/* tabs div end */}

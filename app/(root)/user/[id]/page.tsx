ec516391fb75faef357f1ad1a334f9ba41515377
void <95859137+voiiddxx@users.noreply.github.com>
2024-05-02 16:27:22 +0530

    Added changes

--- app/(root)/user/[id]/page.tsx
+++ app/(root)/user/[id]/page.tsx
@@ -4,6 +4,7 @@ import { Dot, Github, Linkedin, Mail } from 'lucide-react'
 import React from 'react'
 import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs"
 import Link from 'next/link'
+import Image from 'next/image'
 
 
 const page = async({
@@ -13,7 +14,9 @@ const page = async({
   }}) => {
 
 
-    const user = await getUserUsinguserId(id);
+    const user= await getUserUsinguserId(+id);
+    console.log("This is user.data:" , user.data);
+    
     const res = [1,2,65,4,5];
   return (
    <div className='min-h-screen w-full bg-gradient-to-b from-slate-300 to-white ' >
@@ -25,11 +28,12 @@ const page = async({
     <div className='w-full flex justify-between' >
         <div className='flex gap-4 items-center' >
         <div className='h-[150px] w-[150px] rounded-full bg-white' >
+            <Image src={user.data.avatar} className='h-[150px] w-[150px] object-cover' height={1500} width={1500} alt='userimaeg' />
         </div>
         <div>
-            <h1 className='text-zinc-800 text-4xl font-semibold mt-2' >{user.username}</h1>
+            <h1 className='text-zinc-800 text-4xl font-semibold mt-2' >{user.data.username}</h1>
            <div className='flex gap-2' >
-           <p className='text-xs font-medium bg-slate-200 px-2 py-1 rounded-full mt-1' >nikhildesign00@gmail.com</p>
+           <p className='text-xs font-medium bg-slate-200 px-2 py-1 rounded-full mt-1' >{user.data.email}</p>
            </div>
             <p className='mt-1 font-medium' >I create product that matters | Full stack developers | building saas products</p>
             <div className='flex gap-4 mt-4'  >
@@ -53,35 +57,35 @@ const page = async({
   </TabsList>
   <TabsContent value="account">
     {
-        res.length > 0 && (
-            <div className='flex gap-8 flex-wrap ' >
+        user.data.ownedProducts.length > 0 && (
+            <div className='flex gap-8 flex-wrap  mt-8' >
                 {
-                    res.map((curr:any)=>{
-                        return  <Link href={`/user/${curr.id}`} >
-                        <div className="h-[350px] w-[350px] bg-white rounded-md">
+                    user.data.ownedProducts.map((curr:any)=>{
+                        return  <Link href={`/user.data/${curr.id}`} >
+                        <div className="h-[350px] w-[350px] bg-white rounded-md shadow-[0px_20px_20px_10px_#edf2f7]">
                           {/* upper div */}
                           <div className="h-[150px] w-full rounded-t-md bg-gradient-to-tr from-zinc-900 to-zinc-700 relative">
                             <div className="absolute h-[90px] w-[90px] bg-white rounded-full bottom-[-30px]  left-5 border-solid border-[10px] border-white flex items-center justify-center">
-                              {/* <Image
-                                src={curr.avatar}
+                              <Image
+                                src={curr.productlogo}
                                 className="h-[70px] w-[70px] rounded-full object-cover"
                                 height={1500}
                                 width={1500}
                                 alt="userimage"
-                              /> */}
+                              />
                             </div>
                           </div>
                           
                           {/* bottom div */}
                           <div className="mt-10 px-6" >
                              <div className='flex gap-2' >
-                             <h1 className="text-lg font-semibold text-zinc-950" >Productsphere</h1>
+                             <h1 className="text-lg font-semibold text-zinc-950" >{curr.name}</h1>
                              <div className='bg-yellow-100 flex justify-center text-yellow-700 items-center rounded-full px-2 py-1' >
                                 <Dot absoluteStrokeWidth strokeWidth={5} size={20} />
-                                <p className='text-xs font-medium' >Under development</p>
+                                <p className='text-xs font-medium' >{curr.visibility}</p>
                              </div>
                              </div>
-                              <p className="mt-2 text-sm font-medium text-zinc-600" >Platform to improve the product management practise</p>
+                              <p className="mt-2 text-sm font-medium text-zinc-600" >{curr.detail}</p>
                               <p className="mt-10 text-indigo-700 text-xs" >view more</p>
                           </div>
                         </div>
@@ -92,7 +96,47 @@ const page = async({
         )
     }
   </TabsContent>
-  <TabsContent value="password">Change your password here.</TabsContent>
+  <TabsContent value="password">
+  {
+        user.data.memberOfProducts.length > 0 && (
+            <div className='flex gap-8 flex-wrap  mt-8' >
+                {
+                    user.data.memberOfProducts.map((curr:any)=>{
+                        return  <Link href={`/user.data/${curr.id}`} >
+                        <div className="h-[350px] w-[350px] bg-white rounded-md shadow-[0px_20px_20px_10px_#edf2f7]">
+                          {/* upper div */}
+                          <div className="h-[150px] w-full rounded-t-md bg-gradient-to-tr from-zinc-900 to-zinc-700 relative">
+                            <div className="absolute h-[90px] w-[90px] bg-white rounded-full bottom-[-30px]  left-5 border-solid border-[10px] border-white flex items-center justify-center">
+                              <Image
+                                src={curr.productlogo}
+                                className="h-[70px] w-[70px] rounded-full object-cover"
+                                height={1500}
+                                width={1500}
+                                alt="userimage"
+                              />
+                            </div>
+                          </div>
+                          
+                          {/* bottom div */}
+                          <div className="mt-10 px-6" >
+                             <div className='flex gap-2' >
+                             <h1 className="text-lg font-semibold text-zinc-950" >{curr.name}</h1>
+                             <div className='bg-yellow-100 flex justify-center text-yellow-700 items-center rounded-full px-2 py-1' >
+                                <Dot absoluteStrokeWidth strokeWidth={5} size={20} />
+                                <p className='text-xs font-medium' >{curr.visibility}</p>
+                             </div>
+                             </div>
+                              <p className="mt-2 text-sm font-medium text-zinc-600" >{curr.detail}</p>
+                              <p className="mt-10 text-indigo-700 text-xs" >view more</p>
+                          </div>
+                        </div>
+                        </Link>
+                    })
+                }
+            </div>
+        )
+    }
+  </TabsContent>
 </Tabs>
 
     </div>
